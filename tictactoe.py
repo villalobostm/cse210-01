bd3d48cfa9ba7af89a56cd4f5ef0dfee0b79bd7b
villalobostm <104577024+villalobostm@users.noreply.github.com>
2022-04-29 15:21:20 -0600

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,33 @@
+# CSE 210 | Programming With Classes
+# Tic-Tac-Toe game
+# Author: Thomas Manuel Villalobos Aleman
 
 
+number_square_choose = 0
+
+position_square = int(input("x's turn to choose a square (1-9): "))
+
+
+def main():
+
+    while number_square_choose > 9:
+
+  
+
+
+
+def fill_x():
+
+
+
+def fill_o():
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
